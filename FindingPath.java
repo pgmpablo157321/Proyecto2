790db2c4a94c90fba759aeb8076685f80d0633e6
pgmpablo157321 <30699846+pgmpablo157321@users.noreply.github.com>
2017-10-31 22:01:28 -0500

    Update FindingPath.java

--- FindingPath.java
+++ FindingPath.java
@@ -128,13 +128,13 @@ public class FindingPath {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int a[][]=new int[960][1280];
-		/*for(int i=0;i<a.length;i++){
+		for(int i=0;i<a.length;i++){
 			for(int j=0;j<a[0].length;j++){
 				a[i][j]=(int)(Math.random()*90)+10;
 				System.out.print(a[i][j]+" ");
 			}
 			System.out.println();
-		}*/
+		}
 		System.out.println();
 		System.out.println();
 		int n[][]=horizontal(Path(a, a.length,a[0].length,true),a);
