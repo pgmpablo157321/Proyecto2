753eda67d167ebc1abbfd6186f1758896f19deb4
Diego Alejandro Oquendo Castaño <30699018+DiegoOquendo@users.noreply.github.com>
2017-11-05 22:41:40 -0500

    Update Program.java

--- Program.java
+++ Program.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 import javax.imageio.ImageIO;
 
-public class Methods {
+public class Program {
 
 	public static void imprimir(int[][] a) {
 		for (int i = 0; i < a.length; i++) {
